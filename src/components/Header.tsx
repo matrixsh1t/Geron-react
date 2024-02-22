13c2eec5ef6177a0332df044319579c185e9a6a1
Roman <roman.stupko@gmail.com>
2024-02-22 12:09:30 +0600

    header added

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { Button } from "../shared/ui/Button";
+
+export const Header: FC = function Header() {
+  const handlePress = () => {
+    console.log("click");
+  };
+
+  return (
+    <>
+      <header className={"bg-blue-800 h-20"}>
+        <Button onClick={handlePress}>{"Default button"}</Button>
+        <Button onClick={handlePress} mode={"border"}>{"Border button"}</Button>
+        <Button onClick={handlePress} mode={"dark"}>{"Dark button"}</Button>
+      </header>
+    </>
+  );
+};
